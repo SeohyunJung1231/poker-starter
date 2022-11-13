61727508bc8050edb50ce2184c03056d1fec02e9
seohyun <jeongseohyun31@gmail.com>
2022-11-14 01:28:51 +0900

    POKER-1 add GET random cards api
    * add test code

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,10 @@ dependencies {
 
     // for test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
+    testImplementation("io.mockk:mockk:1.12.4")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/com/jeong/pokerstarter/service/CardServiceTest.kt
@@ -0,0 +1,52 @@
+package com.jeong.pokerstarter.service
+
+import com.jeong.pokerstarter.entity.Card
+import com.jeong.pokerstarter.repository.CardRepository
+import com.jeong.pokerstarter.service.CardService.Companion.CARD_NUMBER
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+internal class CardServiceTest(
+) : BehaviorSpec({
+    val repository = mockk<CardRepository>()
+    val service = CardService(repository)
+
+    val cards = listOf(
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC"
+    )
+    every { repository.findAll() } returns cards.map { Card(type = it) }
+
+    given("invoke findCardsByRandom") {
+        `when`("list 비어있을 때") {
+            val actual = service.findCardsByRandom(except = listOf())
+            then("return 임의의 카드 5장") {
+                actual.distinct().size shouldBe CARD_NUMBER
+                cards.containsAll(actual).shouldBeTrue()
+            }
+        }
+        `when`("3개원소 list 를 주었을 때") {
+            val holdCount = 3
+            val holdCards = cards.shuffled().take(holdCount)
+            val actual = service.findCardsByRandom(except = holdCards)
+            then("return 임의의 카드 2장 && 겹치면 안된다") {
+                actual.distinct().size shouldBe CARD_NUMBER - holdCount
+                holdCards.forEach { actual.contains(it).shouldBeFalse() }
+            }
+        }
+        `when`("5개 원소 list 를 주었을 때") {
+            val holdCount = 5
+            val holdCards = cards.shuffled().take(holdCount)
+            val actual = service.findCardsByRandom(except = holdCards)
+            then("return 카드 아무것도 주지 않는다.") {
+                actual.size shouldBe 0
+            }
+        }
+    }
+})
